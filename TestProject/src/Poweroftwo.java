8d27b5612b6c850b14633bc55216d4bf4fadcb29
sourish <sourish@DESKTOP-MDKUKL6>
2020-03-07 14:48:31 -0800

    test

---
+++ TestProject/src/Poweroftwo.java
@@ -0,0 +1,16 @@
+
+public class Poweroftwo {
+   public void isPowerOfTwo(int n) {
+	   
+	  System.out.println((int)Math.pow(2, n));
+        
+    }
+   public static void main(String[] args){
+	   int a=3;
+	   
+	   Poweroftwo i=new Poweroftwo();
+	   i.isPowerOfTwo(a);
+	   
+   }
+
+}
